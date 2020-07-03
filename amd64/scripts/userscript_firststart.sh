05519c547caf8dfc89e7a0db7d7046d99ab594e5
Samuel-0-0 <imhsaw@gmail.com>
2020-07-03 15:16:17 +0800

    Update and rename userscript_firststart_example.sh to userscript_firststart.sh

--- amd64/scripts/userscript_firststart.sh
+++ amd64/scripts/userscript_firststart.sh
@@ -9,4 +9,8 @@ echo ' '
 echo "I'm your startscript userscript_firststart.sh. I will run only on the FIRST startup of the container."
 echo ' '
 
+sudo sed -i 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+
+npm config set registry http://registry.npm.taobao.org/
+
 exit 0
